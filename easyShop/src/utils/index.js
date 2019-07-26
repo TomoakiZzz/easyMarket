cb9adec63c768809833a8ac2e089663abb05a9b9
xiemei <799770735@qq.com>
2019-07-27 07:58:50 +0800

    本地存储

--- easyShop/src/utils/index.js
+++ easyShop/src/utils/index.js
@@ -1,11 +1,7 @@
 const key = 'x-nideshop-token'
 // 设置cookie, 过期时间为十个小时
 export function setToken(val){
-  let date = new Date();
-  let expires = date.getTime() + 10*60*60*1000;
-  date.setTime(expires);
-
-  window.localStorage.setItem(key, val, {expires: date})
+  window.localStorage.setItem(key, val)
 }
 
 // 读取cookie
