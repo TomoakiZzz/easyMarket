3199b5705292688139b436cd74633fa950f4dc3e
wzh <972036796@qq.com>
2019-07-26 15:54:55 +0800

    首页渲染完毕

---
+++ easyShop/src/components/head/head.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Head extends Component {
+    render() {
+        return (
+            <header>
+                <div><i></i></div>
+                <div></div>
+                <div></div>
+            </header>
+        )
+    }
+}
