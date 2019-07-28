6327a30fa7da4822f8334b44c3bf317eb8d0fa59
xiemei <799770735@qq.com>
2019-07-28 21:32:57 +0800

    修改

--- easyShop/src/services/index.js
+++ easyShop/src/services/index.js
@@ -1,5 +1,4 @@
 export * from "./login"
-export * from './my'
 export * from "./home"
 export * from './myCollection'
 export * from "./commodity"
--- easyShop/src/store/index.js
+++ easyShop/src/store/index.js
@@ -1,20 +1,17 @@
 // 引入模块
 import Login from './modules/login'
-import My from './modules/my'
 import Home from "./modules/home"
 import MyCollection from './modules/myCollection'
 import Commodity from "./modules/commodity"
 import Address from './modules/address'
 // 实例化模块
 const login = new Login
-const my = new My
 const home = new Home
 const myCollection = new MyCollection
 const commodity = new Commodity
 const address = new Address
 export default{
     login,
-    my,
     home,
     myCollection,
     commodity,
