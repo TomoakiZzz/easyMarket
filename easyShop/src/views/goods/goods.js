966a1add59759511be45d0afdd015b57f687f088
wzh <972036796@qq.com>
2019-07-29 11:56:04 +0800

    新增商品页面

---
+++ easyShop/src/views/goods/goods.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {inject,observer} from "mobx-react"
+import Head from "../../components/head/head"
+class goods extends Component {
+    render() {
+        return (
+            <div className="noTabPageContent">
+                <Head/>
+                <div className="goodsPage">
+                    
+                </div>
+            </div>
+        )
+    }
+}
+export default goods
